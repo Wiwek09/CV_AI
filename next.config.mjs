1e56b92fdf9f7c07d6e1f76fc6cf743241f475e2
Wiwek09 <kcbibekmail@gmail.com>
2024-12-01 14:53:10 +0545

    changes live to test environment for image

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,23 @@ const nextConfig = {
 
     return config;
   },
+  // Production
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "cvai.rebuzzpos.com",
+  //       pathname: "/cv_images/**",
+  //     },
+  //   ],
+  // },
+  // Local
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cvai.rebuzzpos.com",
-        pathname: "/cv_images/**", // Path to the images in the backend
+        hostname: "cvai.azurewebsites.net",
+        pathname: "/cv_images/**",
       },
     ],
   },
